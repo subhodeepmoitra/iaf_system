d4474ab8e2043bb94ac1842c199d5cdd2f7b61f2
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2022-02-13 12:17:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" style="background: url(&quot;assets/img/iaf1.jpg&quot;);">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>iaf_home</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="assets/css/Header-Dark.css">
+    <link rel="stylesheet" href="assets/css/Logo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="logo" href="icons/indian-air-force.ico" type="image/x-icon">
+</head>
+
+<body>
+    <header class="header-dark"><nav class="navbar navbar-dark navbar-expand-lg navigation-clean-search">
+    <div class="container"><a class="navbar-brand" href="#">IAF Station Diary</a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div id="navcol-1" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="pdf_generation.pdf">View station report</a></li>
+                <li class="nav-item"><a class="nav-link" href="add_new_station/index.php">Register a new Station</a></li>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Departments </a>
+                    <div class="dropdown-menu"><a class="dropdown-item" href="#">Runway Profiling</a><a class="dropdown-item" href="#">Hanger Profile</a><a class="dropdown-item" href="#">Personnel Management</a><a class="dropdown-item" href="#">Armour Management </a><a class="dropdown-item" href="#">Aircraft Management</a></div>
+                </li>
+            </ul>
+            <nav class="navbar navbar-light navbar-expand-md">
+                <div class="container-fluid">
+                    <div><a id="brand-logo" class="navbar-brand" href="#"> </a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button></div>
+                    <div id="navcol-2" class="collapse navbar-collapse">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item"></li>
+                            <li class="nav-item"></li>
+                            <li class="nav-item"></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <form class="me-auto" target="_self">
+                <div class="d-flex align-items-center"><label class="form-label d-flex mb-0" for="search-field"></label></div>
+            </form><span class="navbar-text"></span><a class="btn btn-light action-button" role="button" href="2_station_login/index.php">Station Login</a>
+        </div>
+    </div>
+</nav>
+        <div class="container hero">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <h1 class="text-center">Station Maintenance Diary</h1><div class="ratio ratio-16x9">
+    <a><p style="color: #fff;  font-family: 'Bitter', serif;">The Air officer has Commanded a Remotely Piloted Aircraft Unit and has 800 hrs as Mission Commander on Searcher MK-II & Heron Aircraft. 
+        His other field appointments include Command of an Air Defence Station, Commandant of Electronic Warfare Range and Command of a frontline fighter base. 
+        Important staff assignments held by him include CATSPAW and Air-I of Operational Commands, Principal Director (Information & Electronic Warfare) at Air HQ, 
+        Chief Staff Officer (Air Vector) at HQ Strategic Forces Command, Assistant Chief of Air Staff  Operations (Air Defence), Commandant of the prestigious College of  Air Warfare.
+        Prior to taking over as AOC-in-C, he was  Senior Air Staff Officer Central Air Command. </p></a>
+<iframe sandbox></iframe></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
